fe237a873bc1717af3b88c87a780899ae4363193
jjesuscastro <jjoesei.castro@gmail.com>
2021-03-28 12:14:59 +0800

    Add audio

--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -7,8 +7,16 @@ public class Interactable : MonoBehaviour
 {
     [HideInInspector]
     public UnityEvent onInteract;
+    public AudioClip interactionSound;
+
     public virtual void Interact()
     {
+        if (interactionSound != null)
+        {
+            AudioSource.PlayClipAtPoint(interactionSound, transform.position);
+            Debug.Log("[Interactable.cs] - Audio played");
+        }
+
         if (onInteract != null)
             onInteract.Invoke();
     }
